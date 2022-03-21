8285afbaba37a4551a05ace0f1bcd9c803e616e0
reza gholami <rgh812003@gmail.com>
2022-03-21 09:06:23 -0700

    blueprint of Player class

---
+++ Player.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Player {
+    String name;
+    int index;
+    double budget = 1500;
+    ArrayList<Integer> properties = new ArrayList<Integer>();
+    ArrayList<Card> cards = new ArrayList<Card>();
+
+//    public double getBudget() {
+//        return budget;
+//    }
+//
+//    public void setBudget(double budget) {
+//        this.budget = budget;
+//    }
+//
+//    public ArrayList<Integer> getProperties() {
+//        return properties;
+//    }
+//
+//    public void setProperties(ArrayList<Integer> properties) {
+//        this.properties = properties;
+//    }
+//
+//    public ArrayList<Card> getCards() {
+//        return cards;
+//    }
+//
+//    public void setCards(ArrayList<Card> cards) {
+//        this.cards = cards;
+//    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(name, player.name);
+    }
+
+}
