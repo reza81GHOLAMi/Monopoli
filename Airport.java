cff60450a5212d593ce644fd1e0bfa0592f2dfd8
reza gholami <rgh812003@gmail.com>
2022-03-21 09:07:03 -0700

    blueprint of Airport class

---
+++ Airport.java
@@ -0,0 +1,15 @@
+public class Airport {
+    void fly(int index,Player player){
+        if (index==player.index){
+            System.out.println("***");
+            return;
+        }
+        if ((index==3 || index==11 || index==20) && player.budget>=50){
+            player.index=index;
+            player.budget-=50;
+            return;
+        }
+        System.out.println("can not fly");
+        //2 dalil vogod dard;
+    }
+}
