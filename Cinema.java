7ec1d1c507dbc18760aaaa4515e637c61415aa58
Ali Savari <AliSavari1079@gmail.com>
2022-03-22 18:49:08 +0430

    Cinema class created

---
+++ Cinema.java
@@ -0,0 +1,14 @@
+public class Cinema extends House implements Exchangeable
+{
+    @Override
+    double getCost()
+    {
+        return 0;
+    }
+
+    @Override
+    public int getPrice()
+    {
+        return 200;
+    }
+}
