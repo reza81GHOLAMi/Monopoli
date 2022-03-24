e77b7dfd39aef4457173011e0ffa238b89e1e2a8
reza gholami <rgh812003@gmail.com>
2022-03-23 21:29:08 -0700

    add metod cinemaCount & action and modify getCost

--- Cinema.java
+++ Cinema.java
@@ -5,11 +5,39 @@ public class Cinema extends House implements Exchangeable
         super(color);
     }
 
-    @Override
-    double getCost()
+    int cinemaCount(String name)
     {
-        return 0;
+        int count = 0;
+        int[] index = {3 , 21 , 14};
+        for (int i:index)
+        {
+            if(Map.getInstance().houses[i].owner.equals(name))
+                count += 1;
+        }
+        return count;
     }
+
+    @Override
+    public double getCost(Player player) // nabayad public bashad
+    {
+        int cost = 0 ;
+        if(!(owner.equals("Banker") || owner.equals(player.name)))
+        {
+            cost = 25*cinemaCount(owner);
+            if(sameColor(owner ,this.color))
+            {
+                cost =cost*2 ;
+            }
+        }
+        return cost;
+    }
+
+    @Override
+    void action(Player player)
+    {
+        player.budget-= getCost(player);
+    }
+
 
     @Override
     public int getPrice()
