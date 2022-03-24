4feeebf5481143f79c89e4d64df796719277fb10
reza gholami <rgh812003@gmail.com>
2022-03-23 19:21:11 -0700

    Add default method 'sameColor'

--- Exchangeable.java
+++ Exchangeable.java
@@ -2,4 +2,21 @@ public interface Exchangeable
 {
     int getPrice();
     double getCost(Player player);
+
+    default boolean sameColor(String name, Color color)
+    {
+        int count = 0;
+        for (int i = 0; i < 24; i++)
+        {
+            if (Map.getInstance().houses[i].owner.equals(name))
+            {
+                if (Map.getInstance().houses[i].color == color)
+                {
+                    count += 1;
+                }
+            }
+        }
+        return count == 3;
+    }
+
 }
